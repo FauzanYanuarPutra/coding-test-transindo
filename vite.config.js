dfa279ba0aed7df27004a5c19e1326f10f0f093b
Fauzanyan <ccdplayerx@gmail.com>
2023-05-26 00:22:30 +0700

    ubah src

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pencarian-data",
+  base: "/coding-test-transindo",
   plugins: [vue()],
   resolve: {
     alias: {
