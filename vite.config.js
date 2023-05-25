08c42004d83e831c63712d78fb36b161316ce963
Fauzanyan <ccdplayerx@gmail.com>
2023-05-25 15:09:15 +0700

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/coding-test-transindo",
   plugins: [vue()],
   resolve: {
     alias: {
