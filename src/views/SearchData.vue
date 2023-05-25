1cc7db4884ad570e329e326c600ed45ff2e0caf6
Fauzanyan <ccdplayerx@gmail.com>
2023-05-25 21:46:44 +0700

    imagess

--- src/views/SearchData.vue
+++ src/views/SearchData.vue
@@ -27,7 +27,7 @@
         <!-- <div class=" lg:hidden foto w-[70px] h-[70px] rounded-full  relative  top-[-60px] mb-[-50px] right-[-20px]"><img :src="'public/images/' + data.logo" alt="{{ data.logo }}"></div> -->
         <div class="mx-5 lg:flex lg:justify-end items-center">
             <div class="informasi flex  w-[100%]">
-            <div class="w-[70px]  lg:w-[100px] lg:relative absolute lg:mt-0 mt-[-60px]  mr-5  h-full"><img :src="'public/images/' + data.logo" alt="{{ data.logo }}"></div>
+            <div class="w-[70px]  lg:w-[100px] lg:relative absolute lg:mt-0 mt-[-60px]  mr-5  h-full"><img :src="'public/' + data.logo" alt="{{ data.logo }}"></div>
               <div>
                   <div class="flex items-center  mt-5 lg:mt-0">
                   <h1 class="   mr-5 text-[18px] primary-color">{{ data.company}}</h1>
